223402eb14500534c2ada3efee927f3a6045530a
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2023-03-12 15:55:18 +0000

    it's alive!

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,72 @@
+import configparser
+import logging
+from typing import TypedDict
+
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+discord.utils.setup_logging()
 
-bot.run('token')
+extensions = tuple(("jishaku",))
+
+configKeys = tuple(("botToken", "dbName"))
+
+logger = logging.getLogger(__name__)
+
+class BotConfig(TypedDict):
+    BotToken: str
+    CommandPrefix: str
+
+class Bot(commands.Bot):
+    engine: AsyncEngine
+    config: BotConfig
+
+    async def setup_hook(self) -> None:
+
+        # load the database!
+        self.engine = create_async_engine("sqlite+aiosqlite:///db.sqlite")
+
+        # load extensions
+        for extension in extensions:
+            try:
+                await bot.load_extension(extension)
+            except Exception:
+                logger.exception(f"Error loading extension {extension}")
+            else:
+                logger.info(f"Extension {extension} loaded")
+
+
+if __name__ == "__main__":
+
+    # load the config!
+    config = configparser.ConfigParser()
+    config.read(('config.default.ini', 'config.ini'))
+    
+    botsection = config["bot"]
+
+    botConfig = BotConfig(
+        BotToken=botsection.get("BotToken"),
+        CommandPrefix=botsection.get("BotPrefix", ">")
+    )
+
+    logger.info(botConfig)
+
+    # initalise the bot!
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    allowedMentions = discord.AllowedMentions.none()
+    allowedMentions.replied_user = True
+
+    bot = Bot(
+        command_prefix=commands.when_mentioned_or(botConfig["CommandPrefix"]),
+        intents=intents,
+        allowed_mentions=allowedMentions
+    )
+
+    bot.config = botConfig
+
+    bot.run(botConfig["BotToken"])
