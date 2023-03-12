818280a7d5eba1f78bad8c6d2712a55f9f7236ca
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2023-03-12 20:32:18 +0000

    implement tag-delete

--- src/tags.py
+++ src/tags.py
@@ -1,8 +1,10 @@
 from typing import List
+import discord
 from discord.ext import commands
 from discord import app_commands
 
 from sqlalchemy import select
+from sqlalchemy import delete
 
 import models
 from bot import Bot
@@ -19,7 +21,7 @@ async def tag_autocomplete(
         return []
 
     async with interaction.client.db_session.begin() as session:
-        tag_query = select(models.Tag).where(models.Tag.server_id == interaction.guild.id).limit(25).with_hint(models.Autoresponse, 'USE INDEX col1_index')
+        tag_query = select(models.Tag).where(models.Tag.server_id == interaction.guild.id).limit(25).with_hint(models.Tag, 'USE INDEX col1_index')
         tags = (await session.execute(tag_query)).scalars().all()
 
     return [
@@ -39,7 +41,7 @@ class Tags(commands.Cog):
             return
 
         async with ctx.bot.db_session.begin() as session:
-            tag_query = select(models.Tag).where(models.Tag.server_id == ctx.guild.id).limit(1).with_hint(models.Autoresponse, 'USE INDEX col1_index')
+            tag_query = select(models.Tag).where(models.Tag.server_id == ctx.guild.id).limit(1).with_hint(models.Tag, 'USE INDEX col1_index')
             retrieved_tag = (await session.execute(tag_query)).scalar_one_or_none()
 
         if retrieved_tag is None:
@@ -52,6 +54,42 @@ class Tags(commands.Cog):
         return await tag_autocomplete(interaction, current)
         
 
+    @app_commands.describe(tag='The tag to create')
+    @commands.guild_only()
+    @commands.hybrid_command(
+        name="tag-create",
+        description="Create a tag!",
+        default_permissions=discord.Permissions.advanced()
+    )
+    async def tag_create(self, ctx: Context, tag: str):
+        if not ctx.guild:
+            return
+        return await ctx.reply("Not implemented yet", ephemeral=True)
+
+    @app_commands.describe(tag='The tag to create')
+    @commands.guild_only()
+    @commands.hybrid_command(
+        name="tag-delete",
+        description="Delete a tag!",
+        default_permissions=discord.Permissions.advanced()
+    )
+    async def tag_delete(self, ctx: Context, tag: str):
+        if not ctx.guild:
+            return
+        
+        async with ctx.bot.db_session.begin() as session:
+            tag_query = delete(models.Tag).where(models.Tag.server_id == ctx.guild.id)
+            result = await session.execute(tag_query)
+
+        if result.rowcount == 0:
+            return await ctx.reply("No tag was found.", ephemeral=True)
+        else:
+            return await ctx.reply("Tag deleted sucessfully.")
+
+    @tag_delete.autocomplete('tag') # type: ignore
+    async def tagdelcmd_autocomplete(self, interaction: Interaction, current: str):
+        return await tag_autocomplete(interaction, current)
+
     @commands.Cog.listener(name="on_message")
     async def autorespond(self, message: Message):
         if not message.guild:
@@ -63,7 +101,9 @@ class Tags(commands.Cog):
             for autoresponse in autoresponses:
                 if autoresponse.phrase in message.content:
                     await session.refresh(autoresponse, ("tag",))
-                    return await message.reply(content=autoresponse.tag.content)
+
+                    if autoresponse.tag:
+                        return await message.reply(content=autoresponse.tag.content)
 
         
         
