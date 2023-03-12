f8a27d1e15009cd5b9384d1fa69d346fbe097efa
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2023-03-12 14:55:50 +0000

    let's get started

---
+++ src/bot.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run('token')
